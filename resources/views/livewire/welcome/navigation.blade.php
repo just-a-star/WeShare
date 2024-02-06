05b49abd9118874bf7547c502228f98c56bc75e0
--global <21523283@students.uii.ac.id>
2024-02-06 23:59:02 +0700

    feat(welcome-page, navbar): add initial welcome page layout and content
    added welcome and navigation pages

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,11 +1,16 @@
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-end z-10">
+<div class="z-10 block gap-2 p-6 text-end sm:right-0 sm:top-0 md:fixed">
+    <x-button lg flat secondary label="Features" href="#">Features</x-button>
+    <x-button lg flat secondary label="FAQ" href="#">FAQ</x-button>
     @auth
-        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Dashboard</a>
+        <a href="{{ url('/dashboard') }}"
+            class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-red-500 dark:text-gray-400 dark:hover:text-white"
+            wire:navigate>Dashboard</a>
     @else
-        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Log in</a>
-
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate>Register</a>
+            <x-button lg flat secondary label="Register" href="{{ url('register') }} " wire:navigate></x-button>
         @endif
+
+        <x-button lg light primary label="Login" href="{{ route('login') }}" wire:navigate>Login</x-button>
     @endauth
+
 </div>
