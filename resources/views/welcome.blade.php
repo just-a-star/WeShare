e040c5084594f96f788d183e1f1ebbb602ecf78f
--global <21523283@students.uii.ac.id>
2024-02-08 23:49:30 +0700

    feat(landing page initial done): finishing initial welcome page layout and content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,47 +10,60 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
-<body class="bg-gray-100 antialiased">
-    <div
-        class="bg-dots-darker dark:bg-dots-lighter container relative mx-auto min-h-screen bg-center p-6 selection:bg-red-500 selection:text-white dark:bg-gray-900 sm:flex sm:items-center sm:justify-center">
+<body class="overflow-x-hidden antialiased">
+    <div class="container relative mx-auto p-6">
         @if (Route::has('login'))
             <livewire:welcome.navigation />
         @endif
-        <section>
-            <div container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0>
+
+
+        <section id="hero">
+            {{-- Container for image & content --}}
+            <div class="container mx-auto flex flex-col-reverse p-6 lg:mb-0 lg:flex-row">
                 {{-- Content --}}
-                <div class="flex flex-col space-y-5 lg:mt-8 lg:w-1/2">
-                    <h1 class="text-center text-3xl font-semibold text-sky-950 lg:text-left lg:text-6xl">Welcome to
-                        WeShare - Your Ultimate Note Sharing App
+                <div class="flex flex-col space-y-10 p-2 lg:mt-16 lg:w-1/2">
+                    <h1 class="text-center text-3xl font-bold text-sky-950 lg:text-left lg:text-6xl">WeShare Your
+                        Ultimate Note Sharing App
                     </h1>
-                    <p class="text-center text-lg text-gray-500 lg:mt-0 lg:text-left lg:text-2xl">Discover a new way to
-                        create, share, and engage with notes like never before.</p>
+                    <p
+                        class="mx-auto max-w-md text-center text-lg text-gray-400 lg:mx-0 lg:mt-0 lg:text-left lg:text-2xl">
+                        Discover a new way
+                        to
+                        create, share, and engage with notes like never before. With WeShare, bring your thoughts to
+                        life, share insights, and collaborate effortlessly with others. </p>
+
+                    {{-- Buttons Hero --}}
+                    <div class="flex w-full items-center justify-center space-x-4 lg:justify-start">
+                        <x-button primary class="text-xl font-semibold" xl light wire:navigate
+                            href="{{ url('register') }}">Sign Up
+                            Now!</x-button>
+                        <x-button secondary class="text-xl font-semibold" xl light wire:navigate
+                            href="{{}}">Learn
+                            More</x-button>
+                    </div>
                 </div>
-                {{-- Buttons Nav --}}
-                <div>Hello</div>
+
+                <div class="relative mx-auto lg:mx-0 lg:mb-0 lg:w-1/2">
+                    <div class="bg-hero"></div>
+                    <img src="{{ asset('images/notes.svg') }}"
+                        class="relative z-10 overflow-x-visible lg:top-24 xl:top-0 xl:w-3/4" overflow-x-visible>
+
+                </div>
+
             </div>
         </section>
 
-        {{-- <div class="p-6 mx-auto max-w-7xl lg:p-8">
-            <div class="flex items-center justify-center">
-                <x-application-logo class="block w-auto h-16 fill-current text-sky-950 dark:text-gray-300" />
-                <p class="p-2 text-xl font-semibold text-center text-sky-950 dark:text-gray-300">WeShare
-                </p>
-            </div>
-
-
-
-            <div class="text-sm text-center text-gray-500 ms-4 dark:text-gray-400 sm:ms-0 sm:text-end">
-                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-            </div>
-        </div> --}}
     </div>
-    </div>
+
+
+
 </body>
 
 </html>
